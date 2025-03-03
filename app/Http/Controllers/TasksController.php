575055df6fcbe94eb5a4f874bb6259fa679726e7
heikki.techacademy <cod3.t3sting@gmail.com>
2025-03-03 22:38:40 +0900

    store/update validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,6 +36,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -72,6 +76,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         $task = Task::findOrFail($id);
 
         $task->content = $request->content;
